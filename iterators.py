f4d5e29948ae7ea29a1c548dbec07388c58d8ffd
Rogelio Valle <evenicervalle@gmail.com>
2022-03-01 18:42:13 -0600

    Iterators and Generators

---
+++ iterators.py
@@ -0,0 +1,62 @@
+import time
+
+class FiboIter():
+
+    def __iter__(self):
+        self.n1 = 0
+        self.n2 = 1
+        self.counter = 0
+        return self
+
+    def __next__(self):
+        if self.counter == 0:
+            self.counter += 1
+            return self.n1
+        elif self.counter == 1:
+            self.counter += 1
+            return self.n2
+        else:
+            self.aux = self.n1 + self.n2
+            # self.n1 = self.n2
+            # self.n2 = self.aux
+            self.n1, self.n2 = self.n2, self.aux #swapping
+            self.counter += 1
+            return self.aux
+
+
+class FiboIterMax():
+
+    def __init__(self, max: int) -> None:
+        self.max = max
+
+    def __iter__(self):
+        self.n1 = 0
+        self.n2 = 1
+        self.counter = 0
+        return self
+
+    def __next__(self):
+        if self.counter == 0:
+            self.counter += 1
+            return self.n1
+        elif self.counter == 1:
+            self.counter += 1
+            return self.n2
+        else:
+            self.aux = self.n1 + self.n2
+            self.n1, self.n2 = self.n2, self.aux #swapping
+
+            if self.aux > self.max:
+                raise StopIteration
+            else:
+                 self.counter += 1
+                 return self.aux        
+
+
+
+
+if __name__ == "__main__":
+    fibonacci = FiboIterMax(100)
+    for element in fibonacci:
+        print(element)
+        #time.sleep(1)                    
